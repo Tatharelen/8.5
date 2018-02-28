f26e3ba2e50345c9a7f3cd4e334ce9cba5272ea8
Michał Sikorski <sikorhurricane@gmail.com>
2018-02-28 10:01:44 +0100

    Calculation of the triangle field

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+var a = prompt('Enter base a');
+	alert('Base a = ' + a);
+	h = prompt('Enter height h');
+	alert('Height h = ' + h);
+	traiangleArea = a*h/2;
+	alert('Traiangle field = ' + traiangleArea); 
+
+console.log('Traiangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + traiangleArea);
